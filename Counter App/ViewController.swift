683fa8acdd79b01d1bcbe9e416f9d4f9919ad8cf
Cyrus Raitava-Kumar <cyrus.raitava@gmail.com>
2018-01-31 21:32:51 +1300

    Initial Commit

---
+++ Counter App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Counter App
+//
+//  Created by Cyrus Raitava-Kumar on 31/01/18.
+//  Copyright © 2018 Cyrus Raitava-Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
